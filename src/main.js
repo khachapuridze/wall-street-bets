9a044f325ce9f8bb3e653964aec4300d574ed8d3
guram_kh <g.khachapuridze@palitra.ge>
2023-02-17 21:55:09 +0400

    fixed issues

--- src/main.js
+++ src/main.js
@@ -5,16 +5,12 @@ import store from './store';
 import './assets/main.scss';
 
 window.onload = function () {
-  document.querySelector('#app').style.opacity = '0';
-  document.querySelector('#app').style.visibility = 'hidden';
-
+  document.querySelector('.loader').style.display = 'none';
 
   //hide the preloader
   setTimeout(() => {
     document.querySelector('.preloader').style.display = 'none';
-    // document.querySelector('#app').style.display = 'block';
-    document.querySelector('#app').style.opacity = '1';
-    document.querySelector('#app').style.visibility = 'visible';
+    document.querySelector('.loader').style.display = 'block';
   }, 1);
 };
 createApp(App).use(store).use(router).mount('#app');
